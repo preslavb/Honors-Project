9f4dd12c4b928323e09c80da0e1d410de7940158
Preslavb <preslavb@abv.bg>
2020-04-01 07:36:22 +0100

    fades and stuff

---
+++ Assets/Scripts/FadeInDialogueContainerScript.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class FadeInDialogueContainerScript : MonoBehaviour
+{
+    private const float FADE_IN_TIME = 1.2f;
+    
+    private CanvasGroup _canvasGroup;
+
+    [SerializeField]
+    private DialogueRunner _dialogueRunner;
+    
+    private void Awake()
+    {
+        _canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        float timer = 0;
+
+        while (timer < FADE_IN_TIME)
+        {
+            timer += Time.deltaTime;
+
+            _canvasGroup.alpha = timer / FADE_IN_TIME;
+            
+            yield return new WaitForEndOfFrame();
+        }
+
+        _canvasGroup.alpha = 1;
+        _dialogueRunner.StartDialogue();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PressEnterToContinueScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Doozy.Engine;
+using Doozy.Engine.Utils;
+using UnityEngine;
+
+public class PressEnterToContinueScript : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown && !Input.GetKeyDown(KeyCode.Escape))
+        {
+            GameEventManager.ProcessGameEvent(new GameEventMessage("Test"), true);
+        }
+    }
+}
