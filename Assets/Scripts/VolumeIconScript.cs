93ac4285409855452b16223711f113f75074260a
Preslavb <preslavb@abv.bg>
2020-04-01 10:17:43 +0100

    Audio Control

---
+++ Assets/Scripts/VolumeIconScript.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Doozy.Engine.UI;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeIconScript : MonoBehaviour
+{
+    private UIButton _button;
+
+    [SerializeField]
+    private AudioMixer _audioMixer;
+
+    private void Awake()
+    {
+        _button = GetComponent<UIButton>();
+    }
+
+    public void UpdateButton(float value)
+    {
+        if (value > -0.05f)
+            _button.TextMeshProLabel.text = "\uf028";
+        else if (value < -79f) 
+            _button.TextMeshProLabel.text = "\uf026";
+        else 
+            _button.TextMeshProLabel.text = "\uf027";
+
+        _audioMixer.SetFloat("MasterVolume", value);
+    }
+}
